6a045e2e263eea6efd94174528581ac08ef1f5d2
mauyaa <mauyabevan@gmail.com>
2025-09-08 05:30:21 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -31,6 +31,15 @@ def login_view(request):
     return render(request, 'relationship_app/login.html', {'form': form})
 
 def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
 
 def logout_view(request):
     logout(request)
