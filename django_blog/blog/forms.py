698e1eaebf8c7cdaa6e70806e4a85b166c1d3126
mauyaa <mauyabevan@gmail.com>
2025-10-06 01:09:25 +0300

    feat: align urls and forms with project spec

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
-from django import forms
+﻿from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from taggit.forms import TagWidget
 
 from .models import Comment, Post
 
@@ -23,6 +24,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'content', 'tags')
+        widgets = {
+            'tags': TagWidget(),
+        }
 
 
 class CommentForm(forms.ModelForm):
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,23 +1,25 @@
+﻿from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     # Auth
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('profile/', views.profile_view, name='profile'),
 
     # Posts
     path('', views.PostListView.as_view(), name='post_list'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 
     # Comments
-    path('posts/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
-    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment_update'),
-    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 
     # Tags & Search
     path('tags/<slug:tag_slug>/', views.posts_by_tag, name='posts_by_tag'),
